9dc5101334063c4e5788348aa15cdb6842de3525
vibe <vitaliy.beletskiy@gmail.com>
2025-12-17 20:22:37 +0100

    feat: add update-contact functionality

--- src/main/java/ui/ConsoleUi.java
+++ src/main/java/ui/ConsoleUi.java
@@ -11,6 +11,7 @@ import data.models.Address;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -31,8 +32,7 @@ public final class ConsoleUi {
             new MenuItem("4", "Search by street name", u -> true, () -> handleSearch(SearchType.BY_STREET)),
             new MenuItem("5", "Free search", u -> true, () -> handleSearch(SearchType.FREE)),
             new MenuItem("6", "Create new contact", User::isAdmin, this::createNewContact),
-            new MenuItem("7", "Update existing contact", User::isAdmin, () -> {
-            }),
+            new MenuItem("7", "Update existing contact", User::isAdmin, this::updateContact),
             new MenuItem("8", "Delete contact", User::isAdmin, this::deleteContact),
             new MenuItem("quit", "Exit program", u -> true, () -> System.exit(0))
     );
@@ -143,31 +143,78 @@ public final class ConsoleUi {
 
     private void deleteContact() {
         println("=== Delete Contact ===");
-        println("Enter last name of contact to delete:");
 
-        String lastName = getNonEmptyStringInput();
-        Contact contact = contactService.searchByLastName(lastName).orElse(null);
+        selectContactFromTheList().ifPresentOrElse(
+                c -> {
+                    println("Are you sure you want to delete this contact? (yes/no)");
+                    print("> ");
+                    String confirmation = scanner.nextLine().trim().toLowerCase();
+                    if (confirmation.equals("yes")) {
+                        contactService.deleteContact(c);
+                        println("Contact deleted successfully.");
+                    } else {
+                        println("Delete cancelled.");
+                    }
+                    waitForEnter();
+                },
+                () -> {}
+        );
+    }
 
-        if (contact == null) {
-            println("No contact found with that last name.");
-            return;
-        }
+    private void updateContact() {
+        println("=== Update Contact ===");
 
-        println("Contact found:");
-        printContact(contact, 1);
+        Contact selected = selectContactFromTheList().orElse(null);
+        if (selected == null) return;
 
-        println("Are you sure you want to delete this contact? (yes/no)");
-        print("> ");
-        String confirmation = scanner.nextLine().trim().toLowerCase();
+        String input;
+        println("Enter new First Name or press Enter to keep current value: " + selected.firstName());
+        input = getStringInput();
+        String firstName = input.isEmpty() ? selected.firstName() : input;
 
-        if (confirmation.equals("yes")) {
-            contactService.deleteContact(contact);
-            println("Contact deleted successfully.");
-        } else {
-            println("Delete cancelled.");
-        }
+        println("Enter new Last Name or press Enter to keep current value: " + selected.lastName());
+        input = getStringInput();
+        String lastName = input.isEmpty() ? selected.lastName() : input;
 
-        waitForEnter();
+        println("Enter new Age or press Enter to keep current value: " + selected.age());
+        int age = getAgeInputOrKeep(selected.age());
+
+        println("Enter new Street Name or press Enter to keep current value: " + selected.address().street());
+        input = getStringInput();
+        String streetName = input.isEmpty() ? selected.address().street() : input;
+
+        println("Enter new House Number or press Enter to keep current value: " + selected.address().house());
+        input = getStringInput();
+        String houseNumber = input.isEmpty() ? selected.address().house() : input;
+
+        println("Enter new Postal Code or press Enter to keep current value: " + selected.address().postalCode());
+        input = getStringInput();
+        String postalCode = input.isEmpty() ? selected.address().postalCode() : input;
+
+        println("Enter new City or press Enter to keep current value: " + selected.address().city());
+        input = getStringInput();
+        String city = input.isEmpty() ? selected.address().city() : input;
+
+        println("Enter new Phone Numbers (comma separated) or press Enter to keep current value: " + selected.phoneNumbers());
+        List<String> listInput = getStringListInput();
+        List<String> phoneNumbers = listInput.isEmpty() ? selected.phoneNumbers() : listInput;
+
+        contactService.updateContact(
+                new Contact(
+                        selected.id(),
+                        firstName,
+                        lastName,
+                        age,
+                        new Address(
+                                streetName,
+                                houseNumber,
+                                postalCode,
+                                city
+                        ),
+                        phoneNumbers
+                )
+        );
+        println("\nContact updated successfully.\n");
     }
 
     private void handleSearch(SearchType searchType) {
@@ -221,6 +268,30 @@ public final class ConsoleUi {
         println("");
     }
 
+    private Optional<Contact> selectContactFromTheList() {
+        List<Contact> contacts = contactService.getAllContacts();
+        printContacts(contacts);
+
+        if (contacts.isEmpty()) {
+            println("\nNo contacts found.\n");
+            waitForEnter();
+            return Optional.empty();
+        }
+
+        while (true) {
+            println("Select number of the contact (or type 'cancel').");
+            String input = getStringInput().toLowerCase();
+            if (input.equals("cancel")) return Optional.empty();
+            try {
+                int number = Integer.parseInt(input);
+                if (number < 1 || number > contacts.size()) throw new IllegalArgumentException();
+                return Optional.ofNullable(contacts.get(number - 1));
+            } catch (Exception e) {
+                println("Invalid input, try again.");
+            }
+        }
+    }
+
     private String getStringInput() {
         print("> ");
         String input = scanner.nextLine();
@@ -244,6 +315,25 @@ public final class ConsoleUi {
             print("> ");
             try {
                 int age = Integer.parseInt(scanner.nextLine().strip());
+                if (age < 0 || age > 130) throw new IllegalArgumentException();
+                return age;
+            } catch (Exception e) {
+                println("Enter a valid age.");
+            }
+        }
+    }
+
+    private int getAgeInputOrKeep(int currentAge) {
+        while (true) {
+            print("> ");
+            String input = scanner.nextLine().strip();
+
+            if (input.isEmpty()) {
+                return currentAge;
+            }
+
+            try {
+                int age = Integer.parseInt(input);
                 if (age < 0 || age > 130) throw new IllegalArgumentException();
                 return age;
             } catch (Exception e) {
